b37f951b1e1abbcead63feba9b09fc43d887e288
Your Name <your.email@example.com>
2024-04-28 23:52:03 +0600

    updated settings

--- Happy_shoping/settings.py
+++ Happy_shoping/settings.py
@@ -20,12 +20,13 @@ SECRET_KEY = 'django-insecure-zr5!2tidpw%4ymz9d*mi-$fv*+q)s=yl#tckc_ou_!3ux2ydsk
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = ['https://e-shoping-tkrl.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://smart-shoping-whb0.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', #add new
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +56,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'Happy_shoping.urls'
 
 CORS_ORIGIN_ALLOW_ALL = True
-CSRF_TRUSTED_ORIGINS = ['https://e-shoping-tkrl.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://smart-shoping-whb0.onrender.com']
 
 
 
